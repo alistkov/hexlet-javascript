a040db22d1b12fdb1cd941b9f479e4ca1932a720
Anton Listkov <antonlistkov@gmail.com>
2024-05-18 21:02:11 +0200

    finish isset lesson

---
+++ __tests__/arrays/isset.test.js
@@ -0,0 +1,12 @@
+import { test, expect } from '@jest/globals'
+import get from '../../src/arrays/isset.js'
+
+test('isset', () => {
+  const collection = [1, 2, 3, 4]
+
+  expect(get([], 1)).toBeNull()
+  expect(get(collection, 5, 'default')).toEqual('default')
+  expect(get(collection, 0)).toBe(1)
+  expect(get(collection, 1, 'default')).toBe(2)
+  expect(get(collection, -1, 'default')).toBe('default')
+})
---
+++ src/arrays/isset.js
@@ -0,0 +1,9 @@
+const get = (collection, index, defaulValue = null) => {
+  const lastIndex = collection.length - 1
+  if (index < 0 || index > lastIndex) {
+    return defaulValue
+  }
+  return collection[index]
+}
+
+export default get
