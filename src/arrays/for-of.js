a2ee60e4445adf23ef10450a7a904b27dd9b30f1
Anton Listkov <antonlistkov@gmail.com>
2024-05-20 19:33:03 +0200

    finish for of lesson

---
+++ src/arrays/for-of.js
@@ -0,0 +1,14 @@
+/* eslint no-restricted-syntax: ["off", "ForOfStatement"] */
+const calculateAverage = (collection) => {
+  if (collection.length === 0) {
+    return null
+  }
+
+  let sum = 0
+  for (const item of collection) {
+    sum += item
+  }
+  return sum / collection.length
+}
+
+export default calculateAverage
