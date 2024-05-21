a44b84c5cca7af67e4e6afa7082bf65e844fdde8
Anton Listkov <antonlistkov@gmail.com>
2024-05-21 14:11:50 +0200

    finish nested arrays lesson

---
+++ __tests__/arrays/nested-arrays.test.js
@@ -0,0 +1,52 @@
+import { describe, expect, it } from '@jest/globals'
+import getSuperSeriesWinner from '../../src/arrays/nested-arrays.js'
+
+describe('getSuperSeriesWinner', () => {
+  it('canada', () => {
+    const scores = [
+      [3, 7],
+      [4, 1],
+      [4, 4],
+      [3, 5],
+      [4, 5],
+      [3, 2],
+      [4, 3],
+      [6, 5]
+    ]
+
+    const result = getSuperSeriesWinner(scores)
+    expect(result).toBe('canada')
+  })
+
+  it('ussr', () => {
+    const scores = [
+      [3, 3],
+      [4, 1],
+      [5, 8],
+      [5, 5],
+      [2, 3],
+      [2, 5],
+      [4, 4],
+      [2, 3]
+    ]
+
+    const result = getSuperSeriesWinner(scores)
+    expect(result).toBe('ussr')
+  })
+
+  it('draw', () => {
+    const scores = [
+      [3, 2],
+      [4, 1],
+      [5, 8],
+      [5, 5],
+      [2, 2],
+      [2, 4],
+      [4, 2],
+      [2, 4]
+    ]
+
+    const result = getSuperSeriesWinner(scores)
+    expect(result).toBeNull()
+  })
+})
---
+++ src/arrays/nested-arrays.js
@@ -0,0 +1,15 @@
+/* eslint no-restricted-syntax: ["off", "ForOfStatement"] */
+const getSuperSeriesWinner = (scores) => {
+  let result = 0
+
+  for (const score of scores) {
+    const [canadaScore, ussrScore] = score
+    result += Math.sign(canadaScore - ussrScore)
+  }
+  if (result === 0) {
+    return null
+  }
+  return result > 0 ? 'canada' : 'ussr'
+}
+
+export default getSuperSeriesWinner
