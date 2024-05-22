ca5216cd2221cfea1da56f82032cfa992a783bf9
Anton Listkov <antonlistkov@gmail.com>
2024-05-22 15:19:46 +0200

    finish sorting lesson

---
+++ src/arrays/sorting.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-param-reassign */
+const bubbleSort = (collection) => {
+  let steps = collection.length - 1
+  let swapped
+
+  do {
+    swapped = false
+
+    for (let i = 0; i <= steps; i += 1) {
+      if (collection[i] > collection[i + 1]) {
+        const tmp = collection[i]
+        collection[i] = collection[i + 1]
+        collection[i + 1] = tmp
+        swapped = true
+      }
+    }
+    steps -= 1
+  } while (swapped)
+  return collection
+}
+
+export default bubbleSort
