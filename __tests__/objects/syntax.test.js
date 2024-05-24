00b03bb6fa312651b92e0153f12942e082c65be9
Anton Listkov <antonlistkov@gmail.com>
2024-05-24 12:09:09 +0200

    finish syntax lesson

---
+++ __tests__/objects/syntax.test.js
@@ -0,0 +1,11 @@
+import { test, expect } from '@jest/globals'
+import getJsonFileData from '../../src/objects/syntax.js'
+
+test('getJsonFileData', () => {
+  expect(JSON.stringify(getJsonFileData())).toEqual(
+    JSON.stringify({
+      file: 'src/objects.js',
+      config: true,
+    })
+  )
+})
---
+++ src/objects/syntax.js
@@ -0,0 +1,6 @@
+const getJsonFileData = () => ({
+  file: 'src/objects.js',
+  config: true,
+})
+
+export default getJsonFileData
