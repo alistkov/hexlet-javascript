d91677b201f8f02fce2275e804d33cc4a18d102b
alistkov <antonlistkov@gmail.com>
2025-09-21 02:50:09 +0200

    test commit

--- test/sum.test.js
+++ test/sum.test.js
@@ -1,6 +1,10 @@
 import { expect, it } from 'vitest';
-import { sum } from '../src/sum.js';
+import { multiply, sum } from '../src/sum.js';
 
 it('sum', () => {
   expect(sum(2, 3)).toBe(5);
+});
+
+it('multiply', () => {
+  expect(multiply(2, 2)).toBe(4);
 });
