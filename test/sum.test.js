b544434ccc4bcebb882a7e303dc2d90d30778ab8
alistkov <antonlistkov@gmail.com>
2025-09-21 02:14:59 +0200

    install eslint

--- test/sum.test.js
+++ test/sum.test.js
@@ -1,6 +1,6 @@
-import { test, expect } from 'vitest';
-import { sum } from '../src/sum.js'
+import { expect, it } from 'vitest';
+import { sum } from '../src/sum.js';
 
-test('sum', () => {
-  expect(sum(2, 3)).toBe(5)
-})
+it('sum', () => {
+  expect(sum(2, 3)).toBe(5);
+});
