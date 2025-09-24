7358d81efd735af52d7df1615291b29469e2b7a0
alistkov <antonlistkov@gmail.com>
2025-09-25 00:00:30 +0200

    finish levels lesson

--- src/data-abstraction/points.js
+++ src/data-abstraction/points.js
@@ -7,6 +7,26 @@ export const getX = point => point.x
 
 export const getY = point => point.y
 
+export function getQuadrant(point) {
+  const x = getX(point)
+  const y = getY(point)
+
+  if (x > 0 && y > 0) {
+    return 1
+  }
+  if (x < 0 && y > 0) {
+    return 2
+  }
+  if (x < 0 && y < 0) {
+    return 3
+  }
+  if (x > 0 && y < 0) {
+    return 4
+  }
+
+  return null
+}
+
 export function calculateDistance(point1, point2) {
   const [x1, y1] = point1
   const [x2, y2] = point2
