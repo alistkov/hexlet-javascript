63b8e1b9ee4ef7cc8dac223d09c5e34621692ce2
alistkov <antonlistkov@gmail.com>
2025-09-24 23:18:33 +0200

    abstractions lesson

--- src/data-abstraction/points.js
+++ src/data-abstraction/points.js
@@ -1,3 +1,12 @@
+export function makeDecartPoint(x, y) {
+  const point = { x, y }
+  return point
+}
+
+export const getX = point => point.x
+
+export const getY = point => point.y
+
 export function calculateDistance(point1, point2) {
   const [x1, y1] = point1
   const [x2, y2] = point2
